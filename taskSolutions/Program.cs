fd136c645f92eef8d488eea3960762a21bdc6ccc
Dima Ballanar <D.Ballanar@gmail.com>
2022-10-24 15:57:03 +0300

    решение задач if else switch

--- taskSolutions/Program.cs
+++ taskSolutions/Program.cs
@@ -122,21 +122,43 @@
 // месяца (2 способа)
 
 // 1 способ
-Console.WriteLine("Введите порядковый номер месяца в году(1-12):");
+// Console.WriteLine("Введите порядковый номер месяца в году(1-12):");
+// int nomerMesiaca=Convert.ToInt32(Console.ReadLine());
+// if(nomerMesiaca<=2 || nomerMesiaca==12)
+// {
+//     Console.WriteLine("пора года - зима");
+// }
+// else if(nomerMesiaca>2 && nomerMesiaca<6)
+// {
+//     Console.WriteLine("пора года - весна");
+// }
+// else if(nomerMesiaca>6 && nomerMesiaca<9)
+// {
+//     Console.WriteLine("пора года - лето");
+// }
+// else
+// {
+//     Console.WriteLine("пора года - осень");
+// }
+
+// 2 способ
+ Console.WriteLine("Введите порядковый номер месяца в году(1-12):");
 int nomerMesiaca=Convert.ToInt32(Console.ReadLine());
-if(nomerMesiaca<=2 || nomerMesiaca==12)
+string poraGoda= nomerMesiaca switch
 {
-    Console.WriteLine("пора года - зима");
-}
-else if(nomerMesiaca>2 && nomerMesiaca<6)
-{
-    Console.WriteLine("пора года - весна");
-}
-else if(nomerMesiaca>6 && nomerMesiaca<9)
-{
-    Console.WriteLine("пора года - лето");
-}
-else
-{
-    Console.WriteLine("пора года - осень");
-}
+    0 or >7 or <0 =>"попробуй еще раз",
+    1=> "зима",
+    2=> "зима",
+    3=>"весна",
+    4=>"весна",
+    5=>"весна",
+    6=>"лето",
+    7=>"лето",
+    8=>"лето",
+    9=>"осень",
+    10=>"осень",
+    11=>"осень",
+    12=>"зима"
+
+};
+Console.WriteLine(poraGoda);
