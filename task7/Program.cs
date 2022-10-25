18cce21de06cb6844c351905e8036a4f9dad62e1
Dima Ballanar <D.Ballanar@gmail.com>
2022-10-25 11:55:46 +0300

    проработка и набивание руки на задачах

---
+++ task7/Program.cs
@@ -0,0 +1,19 @@
+﻿// zada4i 25.10.2022
+// 1. Объявить оперменные и поменять в Hих значения три раза (string, int, bool, double)
+
+{
+    string stringType1="45grr";
+    Console.WriteLine(stringType1);
+    stringType1=stringType1 +"Weak";
+    Console.WriteLine(stringType1);
+    stringType1=stringType1 +"Wea";
+Console.WriteLine(stringType1);
+stringType1=stringType1+"rotor";
+Console.WriteLine(stringType1);
+}
+
+//2.2. Произвести все все комбинации неявного преобразования (byte, int, long, float, double, decemel)
+
+{
+
+}
