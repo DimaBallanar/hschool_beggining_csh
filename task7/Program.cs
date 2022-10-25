a4f0da712b1a57d889c577ec00c951683faaba3f
Dima Ballanar <D.Ballanar@gmail.com>
2022-10-25 12:14:00 +0300

    проработка и набивание руки на задачах

--- task7/Program.cs
+++ task7/Program.cs
@@ -11,7 +11,36 @@ Console.WriteLine(stringType1);
 stringType1=stringType1+"rotor";
 Console.WriteLine(stringType1);
 }
-
+{
+int intType1=865;
+Console.WriteLine(intType1);
+intType1=intType1-10;
+Console.WriteLine(intType1);
+intType1=intType1/2;
+Console.WriteLine(intType1);
+intType1=intType1*9;
+Console.WriteLine(intType1);
+}
+{
+    bool boolType1=true;
+    Console.WriteLine(boolType1);
+    boolType1=!boolType1;
+    Console.WriteLine(boolType1);
+    boolType1=!boolType1 && !boolType1;
+    Console.WriteLine(boolType1);
+    boolType1=!boolType1 || !boolType1;
+    Console.WriteLine(boolType1);
+}
+{
+double doubleType1=865;
+Console.WriteLine(doubleType1);
+doubleType1=doubleType1-10;
+Console.WriteLine(doubleType1);
+doubleType1=++doubleType1;
+Console.WriteLine(doubleType1);
+doubleType1=doubleType1%2;
+Console.WriteLine(doubleType1);
+}
 //2.2. Произвести все все комбинации неявного преобразования (byte, int, long, float, double, decemel)
 
 {
