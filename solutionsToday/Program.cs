4d9fb4e81d05a76b2c9ecd49613580499173b500
Dima Ballanar <D.Ballanar@gmail.com>
2022-11-14 19:47:37 +0300

    решение задач с типами данных

---
+++ solutionsToday/Program.cs
@@ -0,0 +1,37 @@
+﻿// int number = 5;
+// object item = number;
+// System.Console.WriteLine(item.GetType());
+// string text = "text";
+// item = text;
+// System.Console.WriteLine(item.GetType());
+// char symbol = '?';
+// item = symbol;
+// System.Console.WriteLine(item.GetType());
+// bool value = true;
+// item = value;
+// System.Console.WriteLine(item.GetType());
+
+// object item1 = 5.0;
+// //подходит если вы уверены на 100%, что item это число
+// double number1 = Convert.ToDouble(item1); 
+// object item2 = 4;
+// //происходит проверка на тип данных, если к такому типу данных привести 
+// //возможно возвращаем число, в противном случае значение по умолчанию
+// int number2 = item2 is int number ? number : default;
+
+// два числа с моего ввода, 2 обджжекта ,если 2 стринги то возвращаем сумму их длин, 2 инта -сумму интов, в противном ничего не ввести
+
+// object myItem1 = 64;
+// object stasItem = "daroy otec";
+// if (myItem1 is string string1 && stasItem is string string2)
+// {
+//     System.Console.WriteLine(string1.Length + string2.Length);
+// }
+// else if (myItem1 is int int1 && stasItem is int int2)
+// {
+//     System.Console.WriteLine(int1 + int2);
+// }
+// else
+// {
+//     System.Console.WriteLine("empty");
+// }
