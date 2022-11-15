a53e9c91e5e4ee853d4c713962f1ce571f60f8fc
Dima Ballanar <D.Ballanar@gmail.com>
2022-11-15 17:38:31 +0300

    решение задач с типами данных

---
+++ TypeWork/Program.cs
@@ -0,0 +1,34 @@
+﻿// HomeWOrk from 15.11.2022
+
+
+
+// 1. Пользователь вводит границы интервала из которого программа загадывает число.
+// Написать программу про поиску данного числа. Пользователь пишет число в консоль,
+// программа отвечает. 
+
+{
+    System.Console.WriteLine("введите границы числового диапазона");
+    System.Console.WriteLine("введите минимальный порог:");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("введите максимально допустимое значение");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+    int random = new Random().Next(minValue, maxValue);
+    System.Console.WriteLine("попробуй угадать число,которое я загадал");
+    int tryGuess=Convert.ToInt32(Console.ReadLine());
+    if(random==tryGuess)
+    {
+        System.Console.WriteLine("верно,как ты это сделал?");
+    }
+    else if(tryGuess<=random+10 && tryGuess>=random-10)
+    {
+        System.Console.WriteLine($"ты был близок {random}");
+    }
+    else
+    {
+        System.Console.WriteLine($"не угадал {random}");
+    }
+
+
+
+
+}
