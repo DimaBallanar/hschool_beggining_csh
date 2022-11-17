18f6bfc537f41461bad004425004bc8cdd59dd26
DimaBallanar <D.Ballanar@gmail.com>
2022-11-17 06:28:33 +0300

    решение задач с типами данных

--- TypeWork/Program.cs
+++ TypeWork/Program.cs
@@ -49,47 +49,78 @@
 // 2. Написать программу, которая подсчитывает количество в строке букв, пробелов, цифр и
 // остальных символов.
 
-// {
-System.Console.WriteLine("введите набор текста");
-string? textRandom = Console.ReadLine();
-if (!string.IsNullOrEmpty(textRandom))
 {
-    int liter = 0;
-    int probel = 0;
-    int number = 0;
-    int symbol = 0;
-    for (int i = 0; i < textRandom.Length; i++)
+    System.Console.WriteLine("введите набор текста");
+    string? textRandom = Console.ReadLine();
+    if (!string.IsNullOrEmpty(textRandom))
     {
-        // liter += textRandom[i] is int place ? place : default;
-        // int.TryParse(textRandom[i].ToString,out int kolvo)
-        // if(textRandom[i].GetType()==Int32)
-        if (int.TryParse(textRandom[i].ToString(), out int par))
+        int liter = 0;
+        int probel = 0;
+        int number = 0;
+        int symbol = 0;
+        for (int i = 0; i < textRandom.Length; i++)
         {
-        number += 1;
-    }
-    if (textRandom[i].Equals(typeof(string)))
-    {
-        liter += 1;
-    }
-    if (textRandom[i].Equals(" "))
-    {
-        probel += 1;
+            // liter += textRandom[i] is int place ? place : default;
+            // int.TryParse(textRandom[i].ToString,out int kolvo)
+            // if(textRandom[i].GetType()==Int32)
+            if (int.TryParse(textRandom[i].ToString(), out int par))
+            {
+                number += 1;
+            }
+            if (textRandom[i].Equals(typeof(string)))
+            {
+                liter += 1;
+            }
+            if (textRandom[i].Equals(" "))
+            {
+                probel += 1;
+            }
+            else
+            {
+                symbol += 1;
+            }
+
+        }
+        System.Console.WriteLine(number);
+        System.Console.WriteLine(liter);
+        System.Console.WriteLine(probel);
+        System.Console.WriteLine(symbol);
     }
     else
     {
-        symbol += 1;
+        System.Console.WriteLine("empty");
     }
 
+
 }
-System.Console.WriteLine(number);
-System.Console.WriteLine(liter);
-System.Console.WriteLine(probel);
-System.Console.WriteLine(symbol);
-}
-else
+
+// 3. Пользователь вводит символ. Записать все позиции данного данного символа в строке в
+// массив и вывести через символ, который кажет пользователь.
+
+
+
+
+
+
+// // 4. Пользователь вводит число N, которое является индексом последнего элемента ряда
+// //Фиббоначи. Вывести весь этот ряд записанный в массив.
+
+
 {
-    System.Console.WriteLine("empty");
+    System.Console.WriteLine("введите число N");
+    int N = Convert.ToInt32(Console.ReadLine());
+    // int z=N-1;
+    // int[] result = new int[] { };
+    string result = "";
+    for (int i = 0; i < N; i++)
+    {
+        int t = i;
+        result+=t;
+        int k = t + i;
+        result += k;
+    }
+
+    System.Console.WriteLine(string.Join(' ', result));
+
 }
 
-
-// }
