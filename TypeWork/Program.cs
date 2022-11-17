585e2330dabbe1de7d25bb397aa4b0ac069f1424
DimaBallanar <D.Ballanar@gmail.com>
2022-11-17 05:33:31 +0300

    решение задач с типами данных

--- TypeWork/Program.cs
+++ TypeWork/Program.cs
@@ -1,47 +1,47 @@
-﻿// HomeWOrk from 15.11.2022
+﻿// // HomeWOrk from 15.11.2022
 
 
 
-// 1. Пользователь вводит границы интервала из которого программа загадывает число.
-// Написать программу про поиску данного числа. Пользователь пишет число в консоль,
-// программа отвечает. 
+// // 1. Пользователь вводит границы интервала из которого программа загадывает число.
+// // Написать программу про поиску данного числа. Пользователь пишет число в консоль,
+// // программа отвечает. 
 
-{
-    System.Console.WriteLine("введите границы числового диапазона");
-    System.Console.WriteLine("введите минимальный порог:");
-    int minValue = Convert.ToInt32(Console.ReadLine());
-    System.Console.WriteLine("введите максимально допустимое значение");
-    int maxValue = Convert.ToInt32(Console.ReadLine());
-    int random = new Random().Next(minValue, maxValue);
-    System.Console.WriteLine(random);
-    do
-    {
-        System.Console.WriteLine("попробуй угадать число,которое я загадал");
-        int tryGuess = Convert.ToInt32(Console.ReadLine());
-        if (tryGuess >= minValue && tryGuess <= maxValue && !)
-        {
-            if (random == tryGuess)
-            {
-                System.Console.WriteLine("верно,как ты это сделал?");
-                break;
-            }
-            else if (tryGuess <= random + 10 && tryGuess >= random - 10)
-            {
-                System.Console.WriteLine($"ты был близок");
-            }
-            else
-            {
-                System.Console.WriteLine($"очень холодно {random}");
-            }
+// {
+//     System.Console.WriteLine("введите границы числового диапазона");
+//     System.Console.WriteLine("введите минимальный порог:");
+//     int minValue = Convert.ToInt32(Console.ReadLine());
+//     System.Console.WriteLine("введите максимально допустимое значение");
+//     int maxValue = Convert.ToInt32(Console.ReadLine());
+//     int random = new Random().Next(minValue, maxValue);
+//     System.Console.WriteLine(random);
+//     do
+//     {
+//         System.Console.WriteLine("попробуй угадать число,которое я загадал");
+//         int tryGuess = Convert.ToInt32(Console.ReadLine());
+//         if (tryGuess >= minValue && tryGuess <= maxValue && !)
+//         {
+//             if (random == tryGuess)
+//             {
+//                 System.Console.WriteLine("верно,как ты это сделал?");
+//                 break;
+//             }
+//             else if (tryGuess <= random + 10 && tryGuess >= random - 10)
+//             {
+//                 System.Console.WriteLine($"ты был близок");
+//             }
+//             else
+//             {
+//                 System.Console.WriteLine($"очень холодно {random}");
+//             }
 
-        }
-        else
-        {
-            System.Console.WriteLine("вы ввели число за даипазоном");
-        }
-    }
-    while (random >= minValue && random <= maxValue);
-}
+//         }
+//         else
+//         {
+//             System.Console.WriteLine("вы ввели число за даипазоном");
+//         }
+//     }
+//     while (random >= minValue && random <= maxValue);
+// }
 
 
 
@@ -50,26 +50,46 @@
 // остальных символов.
 
 // {
-    System.Console.WriteLine("введите набор текста");
-    string? textRandom = Console.ReadLine();
-    if (!string.IsNullOrEmpty(textRandom))
+System.Console.WriteLine("введите набор текста");
+string? textRandom = Console.ReadLine();
+if (!string.IsNullOrEmpty(textRandom))
+{
+    int liter = 0;
+    int probel = 0;
+    int number = 0;
+    int symbol = 0;
+    for (int i = 0; i < textRandom.Length; i++)
     {
-        int liter = 0;
-        int probel = 0;
-        int number = 0;
-        int symbol = 0;
-        for (int i = 0; i < textRandom.Length; i++)
+        // liter += textRandom[i] is int place ? place : default;
+        // int.TryParse(textRandom[i].ToString,out int kolvo)
+        // if(textRandom[i].GetType()==Int32)
+        if (int.TryParse(textRandom[i].ToString(), out int par))
         {
-            liter += textRandom[i] is int place? place : default;
-            int.TryParse(textRandom[i].ToString,out int kolvo)
-
-        }
-
+        number += 1;
+    }
+    if (textRandom[i].Equals(typeof(string)))
+    {
+        liter += 1;
+    }
+    if (textRandom[i].Equals(" "))
+    {
+        probel += 1;
     }
     else
     {
-        System.Console.WriteLine("empty");
+        symbol += 1;
     }
+
+}
+System.Console.WriteLine(number);
+System.Console.WriteLine(liter);
+System.Console.WriteLine(probel);
+System.Console.WriteLine(symbol);
+}
+else
+{
+    System.Console.WriteLine("empty");
+}
 
 
 // }
