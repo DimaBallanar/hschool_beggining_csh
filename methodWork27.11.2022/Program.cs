117fa3d30d9b0f2e7cad4dac5b592e7fd8657837
Dima Ballanar <D.Ballanar@gmail.com>
2022-11-27 16:17:19 +0300

    решение задач c помощью методов

---
+++ methodWork27.11.2022/Program.cs
@@ -0,0 +1,28 @@
+﻿// using System;
+
+// namespace MyApp // Note: actual namespace depends on the project name.
+// {
+//     internal class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             string firstName = "Emilia";
+//             string lastName = "Clarke";
+//             Console.WriteLine(CreateUser("Emilia", age: 36, lastName: lastName, height: 156, weight: 77));
+//         }
+
+//         static string CreateUser(string firstName, string lastName, int age, int height, double weight)
+//         {
+//             return $"{firstName},{lastName},{age},{height},{weight}";
+//         }
+
+//     }
+// }
+static double CalculateCost(double costItem, double vat = 13, double costumDuty = 5, double Tax = 10)
+{
+    vat = 1 + vat / 100;
+    costumDuty = 1 + costumDuty / 100;
+    Tax = 1 + Tax / 100;
+    return costItem * vat * costumDuty * Tax;
+}
+System.Console.WriteLine(CalculateCost(100, costumDuty: 7));
