b43d9a36aeb0573aa1b73540a460ed6bdc375ddd
Dima Ballanar <D.Ballanar@gmail.com>
2022-12-01 01:33:41 +0300

    решение задач c помощью ref методов

--- RefMethod/Program.cs
+++ RefMethod/Program.cs
@@ -7,7 +7,13 @@ namespace RefMethodsHomeWork
         static void Main(string[] args)
         {
             // Task1();
-            Task2();
+            // Task2();
+            Task3();
+            // Task4();
+            // Task5();
+            // Task6();
+            // Task7();
+            // Task8();
         }
         #region Task1
         //         1. На входе дано три массива заполненных случайными числами. Написать метод 
@@ -41,7 +47,7 @@ namespace RefMethodsHomeWork
 
         #endregion
 
-        #region Task2();
+        #region Task2;
         //         2. Написать метод, который проверяет является ли введнная строка почтовым
         // адрессом и возвращающий домен почты.(out)
 
@@ -98,6 +104,86 @@ namespace RefMethodsHomeWork
         }
 
         #endregion
+
+#region Task3
+// 3. Написать метод, который удаляет из строки заданный символ. Символ является
+// входным параметром-значением. Строка должна быть параметром-ссылкой и
+// результатом. (ref)
+static void Task3()
+{
+    System.Console.WriteLine("введите строку");
+    string? userText=Console.ReadLine();
+    System.Console.WriteLine("какой символ хотите удалить?");
+    char symbol=Convert.ToChar(Console.ReadLine());
+    if(!string.IsNullOrEmpty(userText)&&!string.IsNullOrEmpty(symbol.ToString()))
+    {
+System.Console.WriteLine(Cleaner(symbol,ref userText));
+    }
+    else
+{
+    System.Console.WriteLine("error");
+}
+
+}
+static string Cleaner(char symbol, ref string userText)
+{
+    // userText=userText.Replace(symbol,Convert.ToChar(""));
+    for(int i=0;i<userText.Length;i++)
+    {
+        if(userText[i]!=symbol)
+        {
+            userText+=userText[i];
+        }
+        else
+        {
+            userText+="";
+        }
+    }
+    return userText;
+}
+
+
+#endregion
+
+#region Task4
+// 4. Написать метод выполняющий сложение парметов, если параметры одного типа
+// данных возвращать true и результат сложение, в противном случае false (out)
+
+
+
+#endregion
+
+#region Task5
+// 5. Пользователь вводит строку и разделитель. Написать метод возвращающий
+// наличие в строке разделителя, если разделитель есть вернуть строку разбитую
+// данным разделителем (out)
+
+
+
+#endregion
+
+#region Task6
+// //6. Написать метод возвращающий разность между найбольшим и наименьшим
+// значением из списка. Результат разности возращать через out, метод для поиска
+// минимального и максимального написать с использованием params)
+
+
+#endregion
+
+#region Task7
+// 7. Написать метод который принимает строку, если строка содержит только 0 и 1
+// вернуть true, а 0 и 1 перевести в 10-ную систему счисления, в противном случае
+// false (out)
+
+
+#endregion
+
+#region Task8
+// 8. На входе метода набор 0 и 1 перевести в 16-ную систему счисления (params)
+
+
+#endregion
+
 
     }
 }
