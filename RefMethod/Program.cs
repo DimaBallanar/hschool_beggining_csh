f37f95530ae1a4d5153fce2a32a505c628f1d139
Dima Ballanar <D.Ballanar@gmail.com>
2022-11-30 23:31:04 +0300

    решение задач c помощью ref методов

--- RefMethod/Program.cs
+++ RefMethod/Program.cs
@@ -6,30 +6,53 @@ namespace RefMethodsHomeWork
     {
         static void Main(string[] args)
         {
-            Task1();
+            // Task1();
+            Task2();
         }
         #region Task1
         //         1. На входе дано три массива заполненных случайными числами. Написать метод 
         // по подсчёту общего количества чётных чисел в массивах. (ref)
         static void Task1()
         {
-            int[] massivOne = { 0, 2, 3, 4, 5, 6, 7, 4, 234, 25 };
+            int[] massivOne = { 1, 2, 3, 4, 5, 6, 7, 4, 234, 25 };
             int[] massivSecond = { 2, 1, 4, 5, 66, 9, 8, 4 };
             int[] massivFive = { 4, 6, 6, 7, 78, 88, 96, 6, 4, 7, 6 };
             int counter = 0;
-            System.Console.WriteLine(counter);
+            System.Console.WriteLine(Count(massivOne, ref counter));
+            System.Console.WriteLine(Count(massivSecond, ref counter));
+            System.Console.WriteLine(Count(massivFive, ref counter));
         }
-        void Count(ref int[] )
+        // void Counter(ref int counter)
+        // {
+        //     counter++;
+        // }
+        static int Count(int[] massiv, ref int counter)
         {
             foreach (int t in massiv)
             {
-                if (t % 2 == 0)
+                if (t % 2 == 0 && t != 0)
                 {
-                    counter
+                    counter++;
                 }
             }
+            return counter;
         }
 
+
+        #endregion
+
+        #region Task2();
+        //         2. Написать метод, который проверяет является ли введнная строка почтовым
+        // адрессом и возвращающий домен почты.(out)
+        static void Task2()
+        {
+            System.Console.WriteLine("введите свою почту");
+            string? userMail = Console.ReadLine();
+            if (!string.IsNullOrEmpty(userMail))
+            {
+
+            }
+        }
 
         #endregion
 
