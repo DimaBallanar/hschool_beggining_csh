ed473f6aab2a242d1a21dd0585c9b5df6b178256
Dima Ballanar <D.Ballanar@gmail.com>
2022-12-11 12:07:04 +0300

    игра на поиск загаданного числа

--- GameNumSearch/Program.cs
+++ GameNumSearch/Program.cs
@@ -13,7 +13,23 @@ namespace GameSearch
 // добавить подсказки, в виде "ты рядом", "далек от истины" и прочее,чтобы поиск не затянулся.
 // чтобы помочь,также, создать массив, в который будут записываться числа с "попыток", при этом их сразу надо упорядочивать по возрастанию,чтобы не бегать глазами.
 // Аминь!
+static void Task()
+{
+    System.Console.WriteLine("давайте попробуем отгадать число от 0 до 100");
+int rnd=Randomaizer();
 
+}
+static int Randomaizer()
+{
+    Random rnd=new Random();
+    int numForSearch=rnd.Next(0,100);
+    return numForSearch;
+}
+static int Search(int rnd)
+{
+    int tryCount=0;
+    System.Console.WriteLine();
+}
 
 #endregion
     }
