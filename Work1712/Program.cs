57266dec261ddd4108b9e0e7a7bce2a2b8fc9570
Dima Ballanar <D.Ballanar@gmail.com>
2022-12-17 11:03:34 +0300

    daily

---
+++ Work1712/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TaskHard // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите число 1:");
+            int.TryParse(Console.ReadLine(), out int number1);
+            Console.Write("введите число 2:");
+            int.TryParse(Console.ReadLine(), out int number2);
+
+            int result = number1 + number2;
+            System.Console.WriteLine(result);
+
+
+        }
+    }
+}
