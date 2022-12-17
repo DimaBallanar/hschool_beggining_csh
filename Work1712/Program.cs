7efd5e6945b1ffc8ce9a33cc9490710053e33434
Dima Ballanar <D.Ballanar@gmail.com>
2022-12-17 12:01:28 +0300

    daily

--- Work1712/Program.cs
+++ Work1712/Program.cs
@@ -14,12 +14,20 @@ namespace TaskHard // Note: actual namespace depends on the project name.
             double result = SummDouble(number1, number2);
             System.Console.WriteLine(result);
 
+            Calculate calculate = new Calculate();
+            result = calculate.Result;
+            System.Console.WriteLine(result);
+            System.Console.WriteLine(calculate.Summ(number2));
+            System.Console.WriteLine(calculate.Defination(number2));
+            System.Console.WriteLine(calculate.Division(number2));
+            System.Console.WriteLine(calculate.Multiplication(number2));
 
-            static double SummDouble(double n, double m)
-            {
 
-                return n + m;
-            }
+        }
+        static double SummDouble(double n, double m)
+        {
+
+            return n + m;
         }
     }
 }
